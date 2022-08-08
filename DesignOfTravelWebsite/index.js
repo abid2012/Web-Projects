d7d67f2191b90aed1ea1bb9855af888a733cc666
S M Abiduzzaman <98245641+abid2012@users.noreply.github.com>
2022-08-09 00:28:23 +0600

    Add files via upload

---
+++ DesignOfTravelWebsite/index.js
@@ -0,0 +1,12 @@
+$("#btn-admin").on("click",function(){
+    location.href='https://localhost/php/Khushbu%20SE%20Project/AdminLogin/adminlogin.html';
+    
+})
+
+$("#btn-my-user").on("click",function(){
+    location.href="https://localhost/php/Khushbu%20SE%20Project/UserLogin/userlogin.html"
+})
+
+$("#btn-my-register").on("click",function(){
+    location.href="https://localhost/php/Khushbu%20SE%20Project/UserRegister/userregister.html"
+})
