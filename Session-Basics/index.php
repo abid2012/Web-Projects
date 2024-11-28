739821d1d7ecceaf67e5cc19440a6250b387e158
S M Abiduzzaman <98245641+abid2012@users.noreply.github.com>
2024-11-28 19:26:04 +0600

    Add files via upload

---
+++ Session-Basics/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>SESSION PRACTICE</title>
+</head>
+<body>
+    
+<form class="bg-dark text-white p-5" method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="user">
+    <div id="emailHelp" class="form-text text-white">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="pass">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary" name="login">Login</button>
+</form>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+<?php
+
+session_start();
+
+include "./db.php";
+
+if(isset($_POST["login"])){
+    if($_POST["user"] == ""){
+        echo "Username is required <br>";
+    }
+
+    if($_POST["pass"] == ""){
+        echo "Password is required";
+    }
+
+    else{
+
+        $username = $_POST['user'];  
+        $password = $_POST['pass'];  
+          
+            //to prevent from mysqli injection  
+            $username = stripcslashes($username);  
+            $password = stripcslashes($password);  
+            $username = mysqli_real_escape_string($con, $username);  
+            $password = mysqli_real_escape_string($con, $password);  
+          
+            $sql = "select * from practices where Username = '$username' and Password = '$password'";  
+            $result = mysqli_query($con, $sql);  
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+            $count = mysqli_num_rows($result);  
+              
+            if($count == 1){
+                $_SESSION["usern"] = $_POST['user'];
+                $_SESSION["passw"] = $_POST['pass'];
+                header("location: ./dashboard.php"); 
+            }  
+            else{  
+                echo "<h1> Login failed. Invalid username or password.</h1>";  
+            }     
+
+    }
+}
+
+
+?>
